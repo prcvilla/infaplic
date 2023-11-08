72eb5b11336feb4718949dcaca841aafd9a937d8
Paulo Villa <prcvilla@gmail.com>
2023-11-08 15:12:10 -0300

    fix: hardlinks do projeto e zip download no readme

--- CodeBlocksProject/InfAplicBase/infaplic.h
+++ CodeBlocksProject/InfAplicBase/infaplic.h
@@ -1,1 +1,29 @@
-../../infaplic.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* string;
+
+string get_string(void){
+    char s[255];
+    scanf(" %[^\n]255s",s);
+    return strdup(s);
+}
+
+int get_int(void){
+    int tmp;
+    scanf("%d",&tmp);
+    return tmp;
+}
+
+float get_float(void){
+    float tmp;
+    scanf("%f",&tmp);
+    return tmp;
+}
+
+char get_char(void){
+    char tmp;
+    scanf(" %c",&tmp);
+    return tmp;
+}
